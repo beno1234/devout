fcdc461fd345f84e10272868277a96e6e1f182af
beno1234 <benolopesdias@gmail.com>
2025-02-04 20:06:28 -0300

    algumas melhorias

--- src/app/Home/sectionWelcome/SectionWelcome.tsx
+++ src/app/Home/sectionWelcome/SectionWelcome.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Image from 'next/image';
 import section1 from '/public/imagem-home1.jpg';
 
@@ -6,37 +5,44 @@ const SectionWelcome = () => {
   return (
     <section
       id="services"
-      className="py-12 px-4 sm:px-6 lg:px-8 mx-auto container"
+      className="py-16 container mx-auto px-4 sm:px-6 lg:px-8"
     >
-      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-12 items-center">
-        <div className="text-left space-y-4">
+      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
+        <div className="space-y-6">
           <h2 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-gray-800 uppercase leading-tight">
             Bem-vindo ao <br />
             <span className="text-[#BA9F3C]">Nosso Site</span>
           </h2>
-          <p className="text-gray-500 text-base sm:text-lg lg:text-xl leading-relaxed">
+          <p className="text-gray-600 text-lg leading-relaxed">
             A <strong>Devout Auditoria e Contabilidade Ltda.</strong> oferece
-            aos seus clientes um conjunto de prestação de serviços, que a
+            aos seus clientes um conjunto de prestação de serviços que a
             diferencia no mercado. Com as mais modernas técnicas de
             administração empresarial, coloca à disposição de seus clientes
             serviços da mais alta qualidade, que agregam valores...
           </p>
-          <button className="w-full sm:w-auto bg-[#141e28] text-white rounded-md font-serif text-lg md:text-xl px-6 py-3">
-            Saiba mais
-          </button>
+          <ActionButton text="Saiba mais" />
         </div>
 
-        {/* Imagem */}
         <div className="flex justify-center">
           <Image
             src={section1}
             alt="Atender bem, atender sempre"
-            className="w-full max-w-sm sm:max-w-md lg:max-w-lg h-auto rounded-lg shadow-md"
+            className="w-full max-w-sm sm:max-w-md lg:max-w-lg h-auto shadow-lg"
+            priority
           />
         </div>
       </div>
     </section>
   );
 };
+
+const ActionButton = ({ text }: { text: string }) => (
+  <button
+    className="w-full  bg-[#141e28] text-white rounded-md text-lg px-6 py-3 hover:bg-[#1a2a38] transition"
+    aria-label={text}
+  >
+    {text}
+  </button>
+);
 
 export default SectionWelcome;
--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,73 +1,74 @@
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 
-interface Cards {
-  id: number;
+interface CardProps {
   title: string;
   description: string;
 }
+
+const cards: CardProps[] = [
+  {
+    title: 'Missão',
+    description:
+      'Fornecer apoio técnico e competente, por meio de auditorias contábil, societária e tributária, agregando valores ao patrimônio das empresas, garantindo informações corretas em seus relatórios e tornando seus clientes preparados para crescer com estratégia e com uma operação integrada.',
+  },
+  {
+    title: 'Visão',
+    description:
+      'Ser reconhecida dentre as empresas de auditoria e assessoria contábil, como uma empresa de credibilidade nos negócios, somando à sua ascensão o sucesso do cliente.',
+  },
+  {
+    title: 'Valores',
+    description:
+      'Ética, confidencialidade, responsabilidade, qualidade e segurança nas informações.',
+  },
+];
+
 export default function Sobre() {
-  const cards: Cards[] = [
-    {
-      id: 1,
-      title: 'Missão',
-      description:
-        'Fornecer apoio técnico e competente, por meio de auditorias contábil, societária e tributária, agregando valores ao patrimônio das empresas, garantindo informações corretas em seus relatórios e tornando seus clientes preparados para crescer com estratégia e com uma operação integrada.',
-    },
-    {
-      id: 2,
-      title: 'Visão',
-      description:
-        'Ser reconhecida dentre as empresas de auditoria e assessoria contábil, como uma empresa de credibilidade nos negócios, somando à sua ascensão o sucesso do cliente',
-    },
-    {
-      id: 3,
-      title: 'Valores',
-      description:
-        'Ética, confidencialidade, responsabilidade, qualidade e segurança nas informações.',
-    },
-  ];
   return (
     <>
       <Header />
 
-      <div className="py-16 container mx-auto max-w-4xl">
-        <div>
-          {' '}
-          <h3 className="text-sm font-semibold text-gray-800 leading-relaxed">
+      <main className="py-16 container mx-auto max-w-4xl">
+        <section className="text-gray-800 space-y-6">
+          <p className="text-lg font-semibold leading-relaxed">
             A Devout Auditoria e Contabilidade Ltda oferece aos seus clientes um
-            conjunto de prestação de serviços, que a diferencia no mercado.
-          </h3>
-          <h3 className="text-sm font-semibold text-gray-800 leading-relaxed mt-4"></h3>
-          <h3 className="text-sm font-semibold text-gray-800 leading-relaxed mt-4">
+            conjunto de prestação de serviços que a diferencia no mercado.
+          </p>
+          <p className="text-lg font-semibold leading-relaxed">
             Com as mais modernas técnicas de administração empresarial, coloca à
-            disposição de seus clientes, serviços da mais alta qualidade, que
-            agregam valores, quer seja via redução de custos administrativos e
-            operacionais, bem como pela implantação de soluções criativas e de
-            vanguarda. <br />
+            disposição de seus clientes serviços da mais alta qualidade, que
+            agregam valores, seja via redução de custos administrativos e
+            operacionais, ou pela implantação de soluções criativas e de
+            vanguarda.
+          </p>
+          <p className="text-lg font-semibold leading-relaxed">
             Nossa meta consiste na obtenção de economia tributária e fiscal,
-            através de planejamento tributário e de recuperação de créditos
-            oriundos dos tributos incidentes sobre a atividade industrial,
-            comercial ou de serviços, tais como: ISS, ICMS, PIS/PASEP, COFINS,
-            CSLL, IRPJ, INSS e outros.
-          </h3>
-        </div>
-      </div>
+            através de planejamento tributário e de recuperação de créditos de
+            tributos como ISS, ICMS, PIS/PASEP, COFINS, CSLL, IRPJ, INSS e
+            outros.
+          </p>
+        </section>
 
-      <div className="flex flex-wrap justify-center gap-4 my-8">
-        {cards.map((card) => (
-          <div
-            key={card.id}
-            className="w-96 p-6 border border-b-2 rounded bg-white shadow-[0px_0px_12px_rgba(0,0,0,0.8)] flex flex-col items-center text-center"
-          >
-            <h2 className="text-lg text-[#BA9F3C] mb-2">{card.title}</h2>
-            <p className="text-gray-600 text-sm md:text-base">
-              {card.description}
-            </p>
-          </div>
-        ))}
-      </div>
+        <section className="flex flex-nowrap justify-center gap-4 my-12">
+          {cards.map((card, index) => (
+            <InfoCard
+              key={index}
+              title={card.title}
+              description={card.description}
+            />
+          ))}
+        </section>
+      </main>
+
       <Footer />
     </>
   );
 }
+
+const InfoCard = ({ title, description }: CardProps) => (
+  <article className="w-96 p-6 border rounded bg-white shadow-lg text-center">
+    <h2 className="text-lg text-[#BA9F3C] font-semibold">{title}</h2>
+    <p className="text-gray-600 text-sm md:text-base">{description}</p>
+  </article>
+);
