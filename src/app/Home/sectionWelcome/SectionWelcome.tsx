f2d00252367b52745f40b18029becb999c82ece7
beno1234 <benolopesdias@gmail.com>
2025-02-04 17:52:55 -0300

    responsividade home

--- src/app/Home/sectionWelcome/SectionWelcome.tsx
+++ src/app/Home/sectionWelcome/SectionWelcome.tsx
@@ -4,30 +4,34 @@ import section1 from '/public/imagem-home1.jpg';
 
 const SectionWelcome = () => {
   return (
-    <section id="services" className="py-16  mx-auto container">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
+    <section
+      id="services"
+      className="py-12 px-4 sm:px-6 lg:px-8 mx-auto container"
+    >
+      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-12 items-center">
         <div className="text-left space-y-4">
-          <h2 className="text-4xl font-bold text-gray-800 uppercase leading-tight">
+          <h2 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-gray-800 uppercase leading-tight">
             Bem-vindo ao <br />
             <span className="text-[#BA9F3C]">Nosso Site</span>
           </h2>
-          <p className="text-gray-400">
+          <p className="text-gray-500 text-base sm:text-lg lg:text-xl leading-relaxed">
             A <strong>Devout Auditoria e Contabilidade Ltda.</strong> oferece
             aos seus clientes um conjunto de prestação de serviços, que a
             diferencia no mercado. Com as mais modernas técnicas de
             administração empresarial, coloca à disposição de seus clientes
             serviços da mais alta qualidade, que agregam valores...
           </p>
-          <button className="w-full bg-[#141e28] text-white rounded-md text-left font-serif text-xl pl-10 p-2">
+          <button className="w-full sm:w-auto bg-[#141e28] text-white rounded-md font-serif text-lg md:text-xl px-6 py-3">
             Saiba mais
           </button>
         </div>
 
+        {/* Imagem */}
         <div className="flex justify-center">
           <Image
             src={section1}
             alt="Atender bem, atender sempre"
-            className="w-[32vw] h-[40vh]"
+            className="w-full max-w-sm sm:max-w-md lg:max-w-lg h-auto rounded-lg shadow-md"
           />
         </div>
       </div>
